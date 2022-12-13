58b07235b69a48682f493de59a9b1c6b3ddf6b67
James Tricker <james@strangeworks.com>
2022-12-13 16:43:02 +0000

    finally got the cors header right

--- react/src/RandomNumberGenerator.js
+++ react/src/RandomNumberGenerator.js
@@ -7,7 +7,7 @@ function RandomNumberGenerator() {
     try {
       const response = await fetch('https://api.cnot.xyz/qrng', {
         method: 'POST',
-        mode: 'no-cors',
+        mode: 'cors',
         headers: {
           'Content-Type': 'application/json',
           'Accept': 'application/json',
